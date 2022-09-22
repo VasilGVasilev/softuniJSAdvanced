a215442aa61959f14779394be1ae075c248c34b2
Vasil Vasilev <vaskomeil@gmail.com>
2022-09-22 19:38:39 +0300

    6/7 domManipExe

--- Advanced/domManExe/04. Fill-Dropdown/dropdown.js
+++ Advanced/domManExe/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.querySelector('#newItemText');
+    let newItemValue = document.querySelector('#newItemValue');
+    let select = document.getElementById('menu');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = newItemText.value;
+    optionElement.value = newItemValue.value;
+
+    select.appendChild(optionElement);
+
+    newItemText.value = '';
+    newItemValue.value = '';
 }
--- Advanced/domManipulationLab/07. Shopping-Cart/solution.js
+++ Advanced/domManipulationLab/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+   let arrOfProducts = Array.from(document.querySelectorAll('div.product-line-price')).map(e=>e.textContent);
+   let btnArr = Array.from(document.querySelectorAll('button.add-product'));
+   let checkout = document.querySelector('button.checkout');
+   let textArea = document.querySelector('textarea');
+
+   let totalPrice = 0;
+   let setOfAddedProducts = new Set();
+
+   // adding logic
+   btnArr.forEach(e => e.addEventListener('click', clickMe));
+   function clickMe(event){
+      
+      let nameClicked = event.target.parentElement.parentElement.children[1].children[0].textContent;
+      let priceClicked = Number(event.target.parentElement.parentElement.children[3].textContent);
+
+      totalPrice += Number(priceClicked);
+      setOfAddedProducts.add(nameClicked);
+
+      textArea.textContent += `Added ${nameClicked} for ${priceClicked.toFixed(2)} to the cart.\n`
+   }
+
+   // checkout logic
+   checkout.addEventListener('click', checkoutMe);
+
+   function checkoutMe(){
+      let arr = Array.from(setOfAddedProducts);
+      btnArr.forEach(e => {
+         e.disabled = true; })
+      checkout.disabled = true;
+      textArea.textContent += `You bought ${arr.join(', ')} for ${totalPrice.toFixed(2)}.`
+   }
 }
