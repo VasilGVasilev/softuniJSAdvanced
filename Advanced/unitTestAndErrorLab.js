08f3b6250f295fe846f2b802caaa8cba1811a930
Vasil Vasilev <vaskomeil@gmail.com>
2022-09-30 13:32:22 +0300

    first unit testing lab

---
+++ Advanced/unitTestAndErrorLab.js
@@ -0,0 +1,20 @@
+function subSum(arr, start, end){
+    if(Array.isArray(arr) == false) {
+        return NaN;
+    }
+    if(start < 0){//clamp
+        start = 0;
+    }
+    if(end > arr.length - 1){
+        end = arr.length - 1;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++){
+        sum += Number(arr[i]);
+    }
+    return sum;
+}
+
+
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300));
