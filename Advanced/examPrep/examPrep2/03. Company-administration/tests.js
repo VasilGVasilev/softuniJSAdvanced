2b47b7041842fb09408d8fb62bba49bb8fa066c0
Vasil Vasilev <vaskomeil@gmail.com>
2022-10-19 18:25:11 +0300

    exam prep 2

--- Advanced/examPrep/examPrep2/03. Company-administration/tests.js
+++ Advanced/examPrep/examPrep2/03. Company-administration/tests.js
@@ -1,1 +1,34 @@
-//TODO
+const {companyAdministration} = require('./companyAdministration');
+const {expect} = require('chai');
+describe('Tests', function(){
+    describe('hiringEmployee', function(){
+        it('Programmer 3', ()=>{
+            expect(()=>companyAdministration.hiringEmployee('stoyqn', 'Engineer', 1)).to.be.throw(`We are not looking for workers for this position.`);
+            expect(companyAdministration.hiringEmployee('stoyqn', 'Programmer', 3)).to.be.equal(`stoyqn was successfully hired for the position Programmer.`);
+            expect(companyAdministration.hiringEmployee('stoyqn', 'Programmer', 4)).to.be.equal(`stoyqn was successfully hired for the position Programmer.`);
+            expect(companyAdministration.hiringEmployee('stoyqn', 'Programmer', 2)).to.be.equal(`stoyqn is not approved for this position.`);
+        })
+    })
+    describe('calculateSalary', function(){
+        it('salary', ()=>{
+            expect(()=>companyAdministration.calculateSalary(-10)).to.be.throw("Invalid hours");
+            expect(()=>companyAdministration.calculateSalary('csdc')).to.be.throw("Invalid hours");
+            expect(()=>companyAdministration.calculateSalary([])).to.be.throw("Invalid hours");
+            expect(()=>companyAdministration.calculateSalary(null)).to.be.throw("Invalid hours");
+            expect(companyAdministration.calculateSalary(10)).to.be.equal(150);
+            expect(companyAdministration.calculateSalary(161)).to.be.equal(3415);
+        })
+    })
+    describe('firedEmployee', function(){
+        it('fire', () =>{
+            expect(()=>companyAdministration.firedEmployee('dsf', 'sd')).to.throw("Invalid input");
+            expect(()=>companyAdministration.firedEmployee('dsf', -10)).to.throw("Invalid input");
+            expect(()=>companyAdministration.firedEmployee(["Petar", "Ivan", "George"], 10)).to.throw("Invalid input");
+            expect(()=>companyAdministration.firedEmployee({}, {})).to.throw("Invalid input");
+            expect(()=>companyAdministration.firedEmployee(["Petar", "Ivan", "George"], null)).to.throw("Invalid input");
+            expect(()=>companyAdministration.firedEmployee(["Petar", "Ivan", "George"], -1)).to.throw("Invalid input");
+            expect(companyAdministration.firedEmployee(["Petar", "Ivan", "George"], 1)).to.be.equal("Petar, George");
+        })
+
+    })
+})
