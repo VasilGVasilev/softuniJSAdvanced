2adf2ba36271d1357f904c098025566cf3a54e3c
Vasil Vasilev <vaskomeil@gmail.com>
2022-10-28 19:51:33 +0300

    more exes

--- Application/asyncExe/02.Bus-Schedule/app.js
+++ Application/asyncExe/02.Bus-Schedule/app.js
@@ -1,11 +1,27 @@
 function solve() {
-
+    let arriveBtn = document.getElementById('arrive');
+    let departBtn = document.getElementById('depart');
+    let infoSpan = document.querySelector('.info');
+    let nextStopId = 'depot';
+    let currentStop = '';
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`)
+            .then(res=>res.json())
+            .then(data=>{
+                infoSpan.textContent = 'Next stop ' + data.name;
+                nextStopId = data.next;
+                currentStop = data.name;
+            })
+
+        arriveBtn.disabled=false;
+        departBtn.disabled=true;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoSpan.textContent = 'Arriving at ' + currentStop;
+
+        arriveBtn.disabled=true;
+        departBtn.disabled=false;
     }
 
     return {
--- Application/asyncExe/03.Forecaster/app.js
+++ Application/asyncExe/03.Forecaster/app.js
@@ -1,5 +1,33 @@
-function attachEvents() {
-    console.log("TODO...");
+async function attachEvents() {
+    let input = document.getElementById('location').value;
+    let submitBtn = document.getElementById('submit');
+    let forecastDiv = document.getElementById('forecast');
+    let currentDiv = document.getElementById('current');
+    let upcomingDiv = document.getElementById('upcoming');
+    submitBtn.addEventListener('click', submit)
+    async function submit(){
+        try {
+            let response = await fetch(`http://localhost:3030/jsonstore/forecaster/locations`)
+            let data = await response.json();
+            console.log(data);
+            let cityFound = data.find(e => e.name === input);
+            
+            let currentConditionRes = await fetch(`http://localhost:3030/jsonstore/forecaster/today/${cityFound.code}`);
+            let currentConditionData = await currentConditionRes.json();
+            console.log(currentConditionData);
+
+            let threeDaysConditionRes = await fetch(`http://localhost:3030/jsonstore/forecaster/today/${cityFound.code}`);
+            let threeDaysConditionData = await threeDaysConditionRes.json();
+            console.log(threeDaysConditionData);
+
+            forecastDiv.style.display = 'block';
+
+            
+        } catch (error) {
+            
+        }
+    }
+    
 }
 
 attachEvents();
