e4bc3bbaf20c60f76b073c9483dd41d30689c3f6
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-01 15:06:17 +0200

    exe2 but improved -> delete by using event propagation

--- Application/authenticationExe/02.Phonebook/app.js
+++ Application/authenticationExe/02.Phonebook/app.js
@@ -12,37 +12,26 @@ function attachEvents() {
             .then(res => res.json())
             .then(data => loadData(data))
         function loadData(data) {
-            phonebook.innerHTML = '';
-            let resultArr = Object.entries(data);
-            
+            phonebook.replaceChildren(); // === phonebook.innerHTML = '';
+            let resultArr = Object.values(data);
+            console.log(resultArr);
             resultArr.forEach(element => {
-                let phoneInBook = element[1].phone;
-                let personInBook = element[1].person;
-                let key = element[0];
                 let li = document.createElement('li');
                 let deleteBtn = document.createElement('button');
-                li.textContent = `${personInBook}: ${phoneInBook}`
+
+                li.textContent = `${element.person}: ${element.phone}`
                 deleteBtn.textContent = 'Delete';
+                deleteBtn.setAttribute('id', element._id);
+
                 li.appendChild(deleteBtn);
                 phonebook.appendChild(li);
-                deleteBtn.addEventListener('click', onDelete)
-                //delete
-                function onDelete(e, key){
-                    fetch(`http://localhost:3030/jsonstore/phonebook/${key}`, {
-                        method: 'DELETE',
-                        redirect: 'follow'
-                    });
-                    let li = e.target.parentElement;
-                    li.remove();
-                }
+
             });
         }
         
     })
 
     
-
-
     // create
     createBtn.addEventListener('click', async ()=>{
         let respone = await fetch(`http://localhost:3030/jsonstore/phonebook/`, {
@@ -58,6 +47,16 @@ function attachEvents() {
         loadBtn.click();
         person.value = '';
         phone.value = '';
+    })
+
+
+    //delete
+    phonebook.addEventListener('click', async (e)=>{
+        let currentId = e.target.id;
+        fetch(`http://localhost:3030/jsonstore/phonebook/${currentId}`, {
+            method: 'DELETE'
+        });
+        e.target.parentElement.remove();
     })
 }
 
