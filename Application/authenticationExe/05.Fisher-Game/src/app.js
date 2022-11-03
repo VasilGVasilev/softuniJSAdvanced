5c4b6ecf7a5208cab784cba1e8cf3a62bae29101
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-03 12:11:06 +0200

    register and logout added

--- Application/authenticationExe/05.Fisher-Game/src/app.js
+++ Application/authenticationExe/05.Fisher-Game/src/app.js
@@ -1,5 +1,30 @@
 window.addEventListener('load', loadingHomepage);
 // how is _ownerId generated? so that it can be used for user specific edit
+
+const logoutBtn = document.getElementById('logout');
+
 async function loadingHomepage(){
-    
+    if (sessionStorage.accessToken == null){
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('main').style.display = 'none';
+
+    } else {
+        document.getElementById('guest').style.display = 'none';
+        document.querySelector(".add").disabled = false;
+    }
+    logoutBtn.addEventListener('click', logoutFunc);
+}
+
+async function logoutFunc(){
+    const token = sessionStorage.getItem('accessToken');
+
+    await fetch('http://localhost:3030/users/logout', {
+        headers:{
+            'X-Authorization': token
+        }
+    });
+
+    sessionStorage.clear();
+
+    window.location = 'http://127.0.0.1:5500/authenticationExe/05.Fisher-Game/src/'
 }
--- Application/authenticationExe/05.Fisher-Game/src/register.js
+++ Application/authenticationExe/05.Fisher-Game/src/register.js
@@ -1,1 +1,61 @@
-console.log('TODO:// Implement Register functionality');
+document.getElementById('user').style.display = 'none';
+
+document.querySelector('form').addEventListener('submit', submitRegister);
+
+async function submitRegister(e){
+    e.preventDefault();
+
+    // get input data
+    let formData = new FormData(e.currentTarget);
+    let email = formData.get('email');
+    let password = formData.get('password');
+    let repass = formData.get('rePass');
+
+    try {
+        // error handling
+        if(email === '' && password === '' && repass === ''){
+            throw new Error('All fields are empty!')
+        }
+        if(email === ''){
+            throw new Error('Email field is empty!')
+        }
+        if(password === ''){
+            throw new Error('Password field is empty!')
+        }
+        if(repass === ''){
+            throw new Error('Repeat field is empty!')
+        }
+        if(repass !== password){
+            throw new Error('Repeat and Password do not match!')
+        }
+        
+        // request-response
+        const response = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+
+        if(response.ok == false){
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        let data = await response.json();
+        
+        // browser storage
+        sessionStorage.setItem('accessToken', data.accessToken);
+        sessionStorage.setItem('_id', data._id);
+        
+        // redirect to Homepage
+        window.location = 'http://127.0.0.1:5500/authenticationExe/05.Fisher-Game/src/'
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
