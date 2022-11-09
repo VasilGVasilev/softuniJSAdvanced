b6c9d2e313ac770d430d79bd627a0120349df771
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-09 13:31:37 +0200

    basic logic of SPA intermodule dependency explained below app.js code

--- Application/spaExe/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
+++ Application/spaExe/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
@@ -15,43 +15,54 @@
 // [ ] edit
 // [ ] delete
 
-// import { homePage } from './home.js'
+import { homePage } from './home.js'
 // import { registerPage } from './register.js'
 // import { loginPage } from './login.js'
 // import { createPage } from './create.js'
 import { updateNav } from './util.js'
 
-// const routes = {
-//     '/': homePage,
-//     '/login': loginPage,
-//     '/logout': logout,
-//     '/register': registerPage,
-//     '/create': createPage
-// };
+const routes = {
+    '/': homePage,
+    // '/login': loginPage,
+    // '/logout': logout,
+    // '/register': registerPage,
+    // '/create': createPage
+};
 
-// document.querySelector('nav').addEventListener('click', onNavigate);// navbar clickability
-// document.querySelector('#add-movie-button a').addEventListener('click', onNavigate);
+document.querySelector('nav').addEventListener('click', onNavigate);// navbar clickability
+document.querySelector('#add-movie-button a').addEventListener('click', onNavigate);
 
-// function onNavigate(e){
-//     e.preventDefault();
-//     if(e.target.tagName == 'A' && e.target.href) { //click valid only on <a> and <a>'s having href, thus, excluding 'welcome, guest'
-//         const url = new URL(e.target.href)
-//         let view = routes[url.pathname];
-//         if (typeof view == 'function'){
-//             view();
-//         }
-//     }
-// }
+function onNavigate(e){
+    e.preventDefault();
+    if(e.target.tagName == 'A' && e.target.href) { //click valid only on <a> and <a>'s having href, thus, excluding 'welcome, guest'
+        const url = new URL(e.target.href)
+        let view = routes[url.pathname];
+        if (typeof view == 'function'){
+            view();
+        }
+    }
+}
 
-window.updateNav = updateNav;
+
 
 // Start application in catalog/homePage/ view -> call homePage (which will render view and fetch data to be rendered) and updateNavBar user||guest
+updateNav();
+homePage(); 
+    // initally, app calls homePage and homePage: 1) renders hide/show funcs from utils.js and 2) fetches data to fill in with
+    // after that: USER/GUEST can call loginPage() from app.js navbar, loginPage() calls showView() from util.js
+    // showView() is in login.js and login.js module is a closure that store relevant for showView(section) section in a top-level variable
+    // then you attach eventListeners for the possible clickable events ensuing from logging in and redirect to homePage() and update nav with updateNav();
 
 
-
+// GENERAL COMMENTS:
 
 // Loading module from  was blocked ERROR
 // when first loading > easy solution: change src attributies in index.html to absolute path src="./src/app.js"
 // why happeing > because you load liveserver not on current directory but several directories higher
 // so it starts to search for the src, hrefs there for example Application JS folder and it does not find them
 // better than aboslute path is relative -> solution: save in git, open new folder -> current directory and RELAX :D
+
+// window.updateNav = updateNav; !!!! HACK to test in browser just functionality without rendering
+
+// if you set routes object and import its functions without them being ready, thus, empty modules -> errors that stall the debugging:
+// Uncaught SyntaxError: ambiguous indirect export: loginPage || Uncaught ReferenceError: homePage is not defined
--- Application/spaExe/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/util.js
+++ Application/spaExe/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/util.js
@@ -1,20 +1,39 @@
-// repeating code is here
+// Boilerplate abstraction tool
+    // hide and show funcs for rendering that are used by homePage()
+    // updateNav logic that is used by app.js
+
+const views = [...document.querySelectorAll('.view-section')];
+
+export function hideAll(){
+    views.forEach(v=>v.style.display = 'none')
+}
+
+export function showView(section){
+    hideAll();
+    section.style.display = 'block';
+}
+
+export function spinner(){
+    const element = document.createElement('p');
+    element.innerHTML = 'Loading &hellip;';
+    
+    return element;
+}
 
 export function updateNav(){
-    //check if there is user in sessionStorage
-    // const user = JSON.parse(sessionStorage.getItem('user'));
-    // console.log(user);
-    // const msgContainer = document.getElementById('welcome-msg');
+    // check if there is user in sessionStorage
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    console.log(user);
+    const msgContainer = document.getElementById('welcome-msg');
     
-    // if(user){
-    //     document.querySelectorAll('.user').forEach(e=>e.style.display = 'inline-block');
-    //     document.querySelectorAll('.guest').forEach(e=>e.style.display = 'none'); 
-    //     msgContainer.textContent = `Welcome, ${user.email}`;       
-    // } else {
-    //     document.querySelectorAll('.user').forEach(e=>e.style.display = 'none');
-    //     document.querySelectorAll('.guest').forEach(e=>e.style.display = 'inline-block'); 
-    //     msgContainer.textContent = '';
-    // }
-    console.log('sdfkjfnds');
+    if(user){
+        document.querySelectorAll('.user').forEach(e=>e.style.display = 'inline-block');
+        document.querySelectorAll('.guest').forEach(e=>e.style.display = 'none'); 
+        msgContainer.textContent = `Welcome, ${user.email}`;       
+    } else {
+        document.querySelectorAll('.user').forEach(e=>e.style.display = 'none');
+        document.querySelectorAll('.guest').forEach(e=>e.style.display = 'inline-block'); 
+        msgContainer.textContent = '';
+    }
 }
 
