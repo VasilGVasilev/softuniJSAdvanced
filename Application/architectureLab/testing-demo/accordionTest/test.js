20f30e146d85a432c6e7a116dbef44dd152a7ea2
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-10 14:56:42 +0200

    additional info on playwright

--- Application/architectureLab/testing-demo/accordionTest/test.js
+++ Application/architectureLab/testing-demo/accordionTest/test.js
@@ -9,21 +9,26 @@ let browser, page; //declare reusable vars
 describe('E2E tests', async function(){
     this.timeout(5000);
 
+    // playwright configuration
     before(async () => { browser = await chromium.launch(); }); //initialise browser launch only once before testing
     after(async () => { await browser.close(); }); // close browser after tests against memory leaks
 
     beforeEach(async () => { page = await browser.newPage(); });
     afterEach(async () => { await page.close(); });
 
+    // actual test
     it('works', async () => {
         await page.goto('http://localhost:3000');
         await page.screenshot({ path: 'site.png'});
     })
 })
+// important CSS selector by text content:
+// Case-insensitive       await page.click('text=More') !matches 'Show More'
+// Case-sensitive        await page.click('text="More"') !doesnt match 'Show More' -> only 'More'
 
 // lite-server is started in the directory you want it: see npm install manual -> script for run && node_modules local install
     // lite-server doesnt have the fixed root problem of live-server (installed locally)
-
+    // both lite and live server are bad for testing! use simpler version
 // live-server extension does work with this bug, but lite-server has it:
 
 // Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure "done()" 
