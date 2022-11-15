abbc5929152cfc298e7f099d419201bdd8858bbe
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-15 12:41:47 +0200

    abstracting CRUD operations to use with login(), register(), logout()

--- Application/architectureExe/03.SoftTerest/src/app.js
+++ Application/architectureExe/03.SoftTerest/src/app.js
@@ -1,3 +1,3 @@
-import * as api from './api.js' //if you just want to load file no need for a specific file to be named
+import * as api from './api/users.js' //if you just want to load file no need for a specific file to be named
 
 window.api = api;
