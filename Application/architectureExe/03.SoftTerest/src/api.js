2f5c4470663580c49e5cca74118d3857eb026587
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-15 10:35:15 +0200

    testing api for REST service

---
+++ Application/architectureExe/03.SoftTerest/src/api.js
@@ -0,0 +1,43 @@
+async function request(url, method = 'get', data){ 
+    const options = {
+        method,
+        headers: {}
+    };
+
+
+
+    try {
+        const response = await fetch(url, options);
+
+        if (response.ok != true){
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+    
+        if(response.status == 204){ //logout: The service returns an empty response - if you attempt to parse it as JSON, you will receive an error! 
+            return response;
+        } else {
+            return response.json();
+        }
+    } catch (error) {
+        alert(error.message);
+        throw error; // otherwise the request() will consume the error and return undefined
+    }
+}
+
+window.api = {
+    request
+}
+
+// NB:
+// Default function parameters allow named parameters to be 
+// initialized with default values if no value or undefined is passed.
+// function multiply(a, b = 1) {
+//     return a * b;
+//   }
+  
+//   console.log(multiply(5, 2));
+//   // expected output: 10
+  
+//   console.log(multiply(5));
+//   // expected output: 5
---
+++ Application/architectureExe/03.SoftTerest/src/app.js
@@ -0,0 +1,1 @@
+import './api.js' //if you just want to load file no need for a specific file to be named
