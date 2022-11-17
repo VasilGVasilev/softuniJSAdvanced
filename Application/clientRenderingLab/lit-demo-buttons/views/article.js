f74b76773be5c4b69e5fe7c39fdcd038426a0c50
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-17 17:27:51 +0200

    form with del button with bind and index

--- Application/clientRenderingLab/lit-demo-buttons/views/article.js
+++ Application/clientRenderingLab/lit-demo-buttons/views/article.js
@@ -1,9 +1,9 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-export const articleTemplate = (onDelete, article) => html`
+export const articleTemplate = (onDelete, article, index) => html` //index comes from .map() in app.js
 <article>
     <h2>${article.title}</h2>
-    <button @click=${onDelete}>Delete</button> 
+    <button @click=${() => onDelete(index)}>Delete</button> 
     <div class="content">
         <p>${article.content}</p>
     </div>
@@ -11,4 +11,6 @@ export const articleTemplate = (onDelete, article) => html`
 </article>`;
 
 const footerTemplate = (author) => html`<footer>Author: ${author}</footer>`
-
+// it works with just onDelete(index) in app.js,
+// because lit-html sees index as a truthy value and thus
+// always deletes the first article
