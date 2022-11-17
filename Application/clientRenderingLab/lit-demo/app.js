d5092032e581e026a9dbd0b8d48206643981a001
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-17 11:16:30 +0200

    createArticleTemplate demo updated with lit-html

--- Application/clientRenderingLab/lit-demo/app.js
+++ Application/clientRenderingLab/lit-demo/app.js
@@ -1,19 +1,38 @@
-import {html, render} from 'https://unpkg.com/lit-html?module'; //this is executed by browser
+import { html, render } from 'https://unpkg.com/lit-html?module'; //this is executed by browser
 
-// html === createTemplate from first demo
-// render === arrow function that createTemplate returns
+import { data } from './data.js';
 
-const template = (name) => html`<h2>Hello there, ${name}</h2>`;
+const templateArticle = (article) => html`
+<article>
+<h2>${article.title}</h2> 
+<div class="content">
+<p>${article.content}</p>
+</div>
+<footer>Author: ${article.author}</footer>
+</article>`;
 
 start();
 
 function start(){
     const main = document.querySelector('main');
 
-    const templateResult = template('Azis'); //that returns an object that cannot be attached directly to DOM -> use render
-    
-    render(templateResult, main);
+    // render is not appendChild, more like replaceChildren so it lit-html puts only its latest input, but doesnt replace hardcoded html
+    render(data.map(templateArticle), main)
 }
+
+// you create the template and put in e to use it later
+// for key.value pairing article.title etc when accessing data with array of objects
+// use data.map goes through each object and the (article) => article.title methods fills in the gaps
+
+
+
+
+
+
+// html === createTemplate from first demo
+// render === arrow function that createTemplate returns
+
+
 
 // const name = 'Peter'
 // const age = '19'
