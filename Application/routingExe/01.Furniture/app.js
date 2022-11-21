007aaff6f3567964adc9d3fb24c70ed27ba09f35
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-21 12:59:14 +0200

    navbar user/guest

---
+++ Application/routingExe/01.Furniture/app.js
@@ -0,0 +1,22 @@
+import page from './node_modules/page/page.mjs';
+import { html, render } from './node_modules/lit-html/lit-html.js'
+
+
+
+
+// show user/guest navbat
+export const updateInfo = () => {
+    let userDiv = document.getElementById('user');
+    let guestDiv = document.getElementById('guest');
+
+    if(localStorage.length == 0){
+        userDiv.style.display = 'none';
+        guestDiv.style.display = 'inline';
+    } else {
+        userDiv.style.display = 'inline';
+        guestDiv.style.display = 'none';
+    }
+}
+updateInfo();
+
+page.start();
