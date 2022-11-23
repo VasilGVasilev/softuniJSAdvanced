679cea88f4ce807397b043a5d67409efe33db5e2
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-23 14:16:42 +0200

    explainantion on closure, first-class, higher-order functions, prototype/class inheritance

--- Application/test.js
+++ Application/test.js
@@ -1,11 +1,32 @@
-function foo(arr) {
-    arr = arr.sort((a, b) => a.localeCompare(b))
-    arr.forEach((x, i) => console.log(`${i+1}.${x}`))
+function equasions(){
+    let result = 0;
+    function plusOne(){
+        result++;
+        console.log(result);
+    }
+    function minusOne(){
+        result--;
+        console.log(result);
+    }
+    function print(){
+        console.log(result);
+    }
+    return {
+        plusOne,
+        minusOne,
+        print
+    }
 }
-foo(["John",
 
-"Bob",
+let sum = equasions();
 
-"Christina",
+sum.minusOne();
+sum.plusOne();
+sum.print();
 
-"Ema"])
+// higher-order but here we even have first-class sum = equasions
+// closure idea heap
+// like constructor function of object that uses class sugar syntax
+// advanced objects that are not only consisting of property but methods (first-class)
+// state -> functions -> return functions && constructor -> methods
+// class based - class cannot be object || prototype based = prototype can be object
