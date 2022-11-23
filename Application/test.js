545d802a19af204f5b290382263dc35b576128a6
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-23 14:27:48 +0200

    modules are closures that make it possible to manipulate variables of these modules from the main app.js

--- Application/test.js
+++ Application/test.js
@@ -30,3 +30,30 @@ sum.print();
 // advanced objects that are not only consisting of property but methods (first-class)
 // state -> functions -> return functions && constructor -> methods
 // class based - class cannot be object || prototype based = prototype can be object
+
+// also this is closure:
+
+app.js
+
+import page from '../node_modules/page/page.mjs';
+page('/about', showAbout);
+
+about.js
+
+import { html } from '../../node_modules/lit-html/lit-html.js';
+const aboutTemplate = () => html`
+<h2>About Us</h2>
+<p>Contact: +1-555-6197</p>`;
+
+
+export function showAbout(ctx) {
+    ctx.render(aboutTemplate());
+}
+
+// you invoke showAbout which is imported from about.js module
+// but it is using another function -> aboutTemplate
+// it will be undefined if not for the following:
+
+// about.js is a closure function 
+// it returns the exported showAbout
+// showAbout uses aboutTemplate which is stored in heap via module capability of closures
