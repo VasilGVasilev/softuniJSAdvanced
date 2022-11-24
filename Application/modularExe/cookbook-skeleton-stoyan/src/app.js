5ff35416e808fcd3f774faf70c5482cae79f32cf
Vasil Vasilev <vaskomeil@gmail.com>
2022-11-24 13:10:30 +0200

    catalog detials

--- Application/modularExe/cookbook-skeleton-stoyan/src/app.js
+++ Application/modularExe/cookbook-skeleton-stoyan/src/app.js
@@ -3,9 +3,12 @@ import { updateNav } from './middlewares/navbar.js';
 import { decorateContext } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
 import { homePage } from './views/home.js';
 import { loginPage } from './views/login.js';
 import { registerPage } from './views/register.js';
+import { preload }  from "./middlewares/preload.js"
 
 
 // separation of concerns
@@ -16,5 +19,7 @@ page('/', homePage)
 page('/login', loginPage)
 page('/register', registerPage)
 page('/catalog', catalogPage)
+page('/create', createPage)
+page('/catalog/:id', preload, detailsPage)
 
 page.start();
